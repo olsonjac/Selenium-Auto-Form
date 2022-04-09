15a033c48de55b4eaebc4d584e7ed3eb7557f32a
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-04-09 17:47:59 -0400

    Create form_pro.py

---
+++ form_pro.py
@@ -0,0 +1,51 @@
+import time
+import names
+import random
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+options.add_argument("-incognito")
+service = ChromeService(executable_path='/home/jacob/Downloads/chromedriver')
+driver = webdriver.Chrome(service=service, options=options)
+
+
+
+driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdBt1_lZ6PwozDEBVuUxl50EwEmKW6cKs9gwEpUvMiCkMky3Q/viewform")
+print(driver.title)
+# This located all available checkboxes on the form
+time.sleep(3)
+checkbox1 = driver.find_element(By.CLASS_NAME, "D8bnZd")
+checkbox2 = driver.find_element(By.XPATH, "//*[@id=\"i12\"]/div[3]/div")
+checkbox3 = driver.find_element(By.XPATH, "//*[@id=\"i15\"]/div[3]/div")
+checkbox4 = driver.find_element(By.XPATH, "//*[@id=\"i18\"]/div[3]/div")
+checks = [checkbox1,checkbox2,checkbox3,checkbox4]
+#This locates the name entry text box
+namebox = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+#This locates the submission button
+submitbutton = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
+#Submit another form
+
+rand_name = names.get_full_name()
+
+while True:
+
+
+  namebox.send_keys(rand_name)
+
+  random.choice(checks).click()
+
+  submitbutton.click()
+  time.sleep(0.5)
+  resubmitbutton = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/div[4]/a")
+
+  resubmitbutton.click()
+
+#browser.close()
