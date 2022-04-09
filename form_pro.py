4a66805adb849dba4fc34f2e9fafea67f90c0ad5
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-04-09 18:16:03 -0400

    Update form_pro.py
    Updated to be able to continuously fill out a google form as many times as desired.

--- form_pro.py
+++ form_pro.py
@@ -16,36 +16,34 @@ options.add_argument("-incognito")
 service = ChromeService(executable_path='/home/jacob/Downloads/chromedriver')
 driver = webdriver.Chrome(service=service, options=options)
 
-
-
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdBt1_lZ6PwozDEBVuUxl50EwEmKW6cKs9gwEpUvMiCkMky3Q/viewform")
-print(driver.title)
+def all():
+  driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdBt1_lZ6PwozDEBVuUxl50EwEmKW6cKs9gwEpUvMiCkMky3Q/viewform")
+  print(driver.title)
 # This located all available checkboxes on the form
-time.sleep(3)
-checkbox1 = driver.find_element(By.CLASS_NAME, "D8bnZd")
-checkbox2 = driver.find_element(By.XPATH, "//*[@id=\"i12\"]/div[3]/div")
-checkbox3 = driver.find_element(By.XPATH, "//*[@id=\"i15\"]/div[3]/div")
-checkbox4 = driver.find_element(By.XPATH, "//*[@id=\"i18\"]/div[3]/div")
-checks = [checkbox1,checkbox2,checkbox3,checkbox4]
+  checkbox1 = driver.find_element(By.CLASS_NAME, "D8bnZd")
+  checkbox2 = driver.find_element(By.XPATH, "//*[@id=\"i12\"]/div[3]/div")
+  checkbox3 = driver.find_element(By.XPATH, "//*[@id=\"i15\"]/div[3]/div")
+  checkbox4 = driver.find_element(By.XPATH, "//*[@id=\"i18\"]/div[3]/div")
+  checks = [checkbox1,checkbox2,checkbox3,checkbox4]
 #This locates the name entry text box
-namebox = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
+  namebox = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
 #This locates the submission button
-submitbutton = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
+  submitbutton = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
 #Submit another form
-
-rand_name = names.get_full_name()
-
-while True:
+  time.sleep(1)
+  rand_name = names.get_full_name()
 
 
+  time.sleep(1)
   namebox.send_keys(rand_name)
 
   random.choice(checks).click()
 
   submitbutton.click()
-  time.sleep(0.5)
   resubmitbutton = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/div[4]/a")
 
   resubmitbutton.click()
 
-#browser.close()
+for i in range(10):
+  all()
+
