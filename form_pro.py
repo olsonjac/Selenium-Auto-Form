a01aa36255cbc18178ae35a3fdc4589ebbf57a70
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-04-09 18:45:14 -0400

    Update form_pro.py
    updated comments for clarity

--- form_pro.py
+++ form_pro.py
@@ -8,42 +8,42 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
+#necessary selenium options
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option("useAutomationExtension", False)
 options.add_argument("-incognito")
-service = ChromeService(executable_path='/home/jacob/Downloads/chromedriver')
+service = ChromeService(executable_path='Path_to_chromedriver')
 driver = webdriver.Chrome(service=service, options=options)
-
+#separates the whole process into a function that can be run multiple times
 def all():
+  #This prints the title of the page as a test that the URL is good
   driver.get("https://docs.google.com/forms/d/e/1FAIpQLSdBt1_lZ6PwozDEBVuUxl50EwEmKW6cKs9gwEpUvMiCkMky3Q/viewform")
   print(driver.title)
-# This located all available checkboxes on the form
+  #This located all available checkboxes on the form
   checkbox1 = driver.find_element(By.CLASS_NAME, "D8bnZd")
   checkbox2 = driver.find_element(By.XPATH, "//*[@id=\"i12\"]/div[3]/div")
   checkbox3 = driver.find_element(By.XPATH, "//*[@id=\"i15\"]/div[3]/div")
   checkbox4 = driver.find_element(By.XPATH, "//*[@id=\"i18\"]/div[3]/div")
+  #Creates a list of all 4 checkbox options
   checks = [checkbox1,checkbox2,checkbox3,checkbox4]
-#This locates the name entry text box
+  #This locates the name entry text box
   namebox = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input")
-#This locates the submission button
+  #This locates the submission button
   submitbutton = driver.find_element(By.XPATH, "//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span")
-#Submit another form
   time.sleep(1)
+  #Generates a random first and last name to input
   rand_name = names.get_full_name()
-
-
-  time.sleep(1)
+  #sends the random name to the namebox input
   namebox.send_keys(rand_name)
-
+  #picks a random checkbox from the 4 options
   random.choice(checks).click()
-
+  #submits the form 
   submitbutton.click()
+  #finds and clicks the "submit another form button at the end
   resubmitbutton = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/div[4]/a")
-
   resubmitbutton.click()
-
+# for loop that submits the form as many times as listed
 for i in range(10):
   all()
 
