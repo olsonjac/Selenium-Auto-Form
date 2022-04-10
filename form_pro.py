11cc97871c9d5393d2686d816694c95005807104
Jacob Olson <50496441+olsonjac@users.noreply.github.com>
2022-04-09 21:08:23 -0400

    Update form_pro.py

--- form_pro.py
+++ form_pro.py
@@ -2,8 +2,6 @@ import time
 import names
 import random
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
